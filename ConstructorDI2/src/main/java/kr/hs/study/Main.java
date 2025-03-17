50a54d2a6a928f4f5eefeeea9c35c30bf00cec8d
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-03-17 11:33:58 +0900

    참조형 객체

--- ConstructorDI2/src/main/java/kr/hs/study/Main.java
+++ ConstructorDI2/src/main/java/kr/hs/study/Main.java
@@ -1,5 +1,7 @@
 package kr.hs.study;
 
+import kr.hs.study.beans.TestBean;
+import kr.hs.study.beans.TestBean2;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,5 +9,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+
+        TestBean obj1 = ctx.getBean("t1", TestBean.class);
+        obj1.prData();
+
+        TestBean obj2 = ctx.getBean("t2", TestBean.class);
+        obj2.prData();
+
+        TestBean2 obj3 = ctx.getBean("t3", TestBean2.class);
+        obj3.prData();
+
+        ctx.close();
     }
 }
