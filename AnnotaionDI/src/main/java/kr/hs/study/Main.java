7bda8ff9e4234e57661b7715fde8187eb4738f34
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-03-21 11:43:57 +0900

    Component로 객체 생성하는 동시에 값 넣기

--- AnnotaionDI/src/main/java/kr/hs/study/Main.java
+++ AnnotaionDI/src/main/java/kr/hs/study/Main.java
@@ -1,5 +1,6 @@
 package kr.hs.study;
 
+import kr.hs.study.beans.TestBean1;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,6 +8,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config_annotation.xml");
+
+        TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+        System.out.println(obj1.getData1());
+        System.out.println(obj1.getData2());
+        System.out.println(obj1.getData3());
+
         ctx.close();
     }
 }
