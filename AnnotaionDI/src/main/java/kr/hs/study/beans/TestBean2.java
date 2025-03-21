b80738fafdfa3476ac4fb631d6da8f663fa4fc83
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-03-21 11:54:44 +0900

    Qualifier : 객체가 여러개일 때 객체 이름 지정

--- AnnotaionDI/src/main/java/kr/hs/study/beans/TestBean2.java
+++ AnnotaionDI/src/main/java/kr/hs/study/beans/TestBean2.java
@@ -1,11 +1,13 @@
 package kr.hs.study.beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("t2")
 public class TestBean2 {
     @Autowired
+    @Qualifier("dataBean2")
     private DataBean2 data1;
 
     public DataBean2 getData1() {
